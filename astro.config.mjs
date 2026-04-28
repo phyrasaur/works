a3b33215701267e3c7b71a47eb91f78a88723734
Firas Razak <dev@firas.works>
2026-04-28 22:47:22 +0800

    add squiggle info panel & about section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "PP Neue Montreal",
+      cssVariable: "--font-pp-neue-montreal",
+      options: {
+        variants: [
+          {
+            src: ["./src/fonts/PPNeueMontreal-Book.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+});
