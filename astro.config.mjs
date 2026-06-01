bfd7fb46c932cac3bb499e635373004bc5ced00c
Firas Razak <dev@firas.works>
2026-06-01 18:51:18 +0800

    fix banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   fonts: [
     {
       provider: fontProviders.local(),
