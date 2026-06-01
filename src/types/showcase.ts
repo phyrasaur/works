f623d6960436fb7e119d8dcab007056c1c58ce4c
Firas Razak <dev@firas.works>
2026-06-02 02:46:35 +0800

    fix typography previe card

--- src/types/showcase.ts
+++ src/types/showcase.ts
@@ -2,7 +2,6 @@ export type ShowcaseItem = {
   brand: string;
   title: string;
   service: string;
-  kind: string;
   image: ImageMetadata;
   markImage?: ImageMetadata;
   mark: "hyperprint" | "donaid" | "wishful";
