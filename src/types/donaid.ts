27543ebdf06e3e7000d94f702685f83657410df4
Firas Razak <dev@firas.works>
2026-06-02 05:21:24 +0800

    Refactor DonAid article components

--- src/types/donaid.ts
+++ src/types/donaid.ts
@@ -1,26 +1,6 @@
-export type DonaidFigure = {
-  image: ImageMetadata;
-  title: string;
-  body: string;
-  className?: string;
-};
-
-export type DonaidProblem = {
-  label: string;
-  title: string;
-  body: string;
-};
-
-export type DonaidFeature = {
-  label: string;
-  title: string;
-  body: string;
-  image: ImageMetadata;
-};
-
-export type DonaidInsight = {
-  label: string;
-  image: ImageMetadata;
-  title: string;
-  body: string;
-};
+export type {
+  ArticleFeature as DonaidFeature,
+  ArticleFigure as DonaidFigure,
+  ArticleInsight as DonaidInsight,
+  ArticleNumberedItem as DonaidProblem,
+} from "@/types/article";
